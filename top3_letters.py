94a2220092837d25f6b561032b8d11f5d01f9f83
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-09 01:14:22 -0400

    Update top3_letters.py

--- top3_letters.py
+++ top3_letters.py
@@ -1,23 +1,27 @@
-#given user input of a company name, return the top 3 most common letters
+#given user input of a string, return 3 most commonly repeated letters
+def top():
+    string = input()
 
-string = input("your company name: ")
+    array = sorted(string)
+    unique = list(set(string))
+    counts = []
+    for i in unique:
+        count = 0
+        for letter in array:
+            if letter == i:
+                count+=1
+        counts.append(count)
 
-array = sorted(string)
-unique = list(set(string))
-counts = []
-for i in unique:
-    count = 0
-    for letter in array:
-        if letter == i:
-            count+=1
-    counts.append(count)
-    
-rank = []
-for check in reversed(range(max(counts)+1)):
-    for i in range(len(counts)):
-        if counts[i] == check:
-            rank.append(unique[i])
-            
-top_3 = rank[:3]
-for i in top_3:
-    print(i)
+    rank = []
+    for check in reversed(range(max(counts)+1)):
+        for i in range(len(counts)):
+            if counts[i] == check:
+                rank.append(unique[i])
+
+    top_3 = rank[:3]
+    print("\n")
+    for i in top_3:
+        print(i)
+
+if __name__ == "__main__":
+    top()
